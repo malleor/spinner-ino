676e3efa91cfc6a65b142e4a3815cde3c9a4b4d7
malleor <malleor@gmail.com>
2014-03-29 17:59:50 +0100

    I can control the speed!

--- spinner.ino
+++ spinner.ino
@@ -1,8 +1,10 @@
 const int nleds = 4;
 const int leds[nleds] = { 6, 9, 10, 11 };
 
-const float FREQ = 3, MAX_BRI = 180;
+const float MAX_BRI = 180;
 const float phase[nleds] = { .0, .25, .5, .75 };
+float freq = 2.;
+const float FREQ_BOOST = 1.1;
 
 void setup()  { 
   // reset leds
@@ -18,20 +20,26 @@ void setup()  {
 void loop()  { 
   for(int i=0; i<nleds; ++i) {
     float time = millis() / 1e+3;
-    int brightness = MAX_BRI/2 + MAX_BRI/2*sin(6.283185*( phase[i] + time * FREQ ));
+    int brightness = MAX_BRI/2 + MAX_BRI/2*sin(6.283185*( phase[i] + time * freq ));
     
     // light up the led
     analogWrite(leds[i], brightness);    
   }   
   
-  // wait for 30 milliseconds to see the dimming effect    
   delay(10);                     
   
   // read the encoder
-  int potential = analogRead(0);
-  if(potential > 0) {
-    Serial.print("potential = ");
-    Serial.println(potential);
+  int pot_pos = analogRead(0);
+  int pot_neg = analogRead(1);
+  if(pot_pos > 0) {    
+    freq *= FREQ_BOOST;
+    Serial.print("freq = ");
+    Serial.println(freq);
+  }
+  if(pot_neg > 0) {    
+    freq /= FREQ_BOOST;
+    Serial.print("freq = ");
+    Serial.println(freq);
   }
 }
 
